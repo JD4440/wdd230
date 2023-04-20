6cd24a4926c0beb0d4edae03c4bee3da5dac7ba9
JD4440 <sajkmj@outlook.com>
2023-04-19 22:16:49 -0600

    JS updated

---
+++ scripts/scripts.js
@@ -0,0 +1,11 @@
+const date1 = document.querySelector("#date1");
+
+try {
+    const options = {year: "numeric"};
+
+date1.innerHTML = `<span class="highlight">${new Date().toLocaleDateString("en-US", options)}</span>`;} 
+catch (e) {console.log("Error with code or your browser does not support Locale");}
+
+const modified1 = document.querySelector("#modified1");
+modified1.innerHTML = new Date(document.lastModified);
+
