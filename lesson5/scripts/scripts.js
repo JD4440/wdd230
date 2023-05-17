030bc1e2679d8eea4c557a81950a6a2db0ea6913
JD4440 <sajkmj@outlook.com>
2023-05-16 20:55:33 -0600

    update BOM

--- lesson5/scripts/scripts.js
+++ lesson5/scripts/scripts.js
@@ -2,3 +2,28 @@ const input = document.querySelector('input');
 const button = document.querySelector('button');
 const list = document.querySelector('ul');
 
+button.addEventListener('click', () => {
+    const favoriteChap = input.value;
+    input.value = "";
+
+    const listItem = document.createElement('li');
+    const listText = document.createElement('span');
+    const listBtn = document.createElement('button');
+
+    listBtn.setAttribute("class","delete");
+    const ariaLabel = "delete" + " " + favoriteChap;
+    listBtn.setAttribute("aria-label",ariaLabel);
+
+    listItem.appendChild(listText);
+    listText.textContent = favoriteChap;
+    listItem.appendChild(listBtn);
+    listBtn.textContent = "❌";
+    list.appendChild(listItem);
+
+    listBtn.addEventListener('click', () => {
+        list.removeChild(listItem);
+        })
+    
+
+    input.focus();
+})
