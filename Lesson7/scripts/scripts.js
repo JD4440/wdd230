1c47de3cf69d83ee95936e585138f42611e540ba
JD4440 <sajkmj@outlook.com>
2023-05-30 22:32:01 -0600

    update

--- Lesson7/scripts/scripts.js
+++ Lesson7/scripts/scripts.js
@@ -1,36 +1,37 @@
-// get all imgs with data-src attribute
-const imaginesToLoad = document.querySelectorAll("img[data-src]");
+// Gather the images to load
+let imagesToLoad = document.querySelectorAll("img[data-src]");
 
-//optional parameters being set for the IntersectionalObserver
-const imgOptions = {
-    threshold: 0,
-    rootMargin: "0px 0px 50px 0px"
-};
-
+// Set up the load images function which switches the src and the data-src attributes.
 const loadImages = (image) => {
-    image.setAttribute('src', image.getAttribute('data-src'));
-    image.onload = () => {image.removeAttribute('data-src');};
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
 };
 
-//first check to see if Intersection Observer is supported
-if('IntersectionObserver' in window) {
-    const imgObserver = new IntersectionObserver((items,observer) => {
-        items.forEach((item) => {
-        });
-    }, imgOptions);
 
-    //Loop through each img and check status and load if necessary 
-    imaginesToLoad.forEach((img) => {
-        imgObserver.observe(img);
-    });
-}
-else { // just load ALL images if not supported
-    imaginesToLoad.forEach((img) => {
-        loadImages(img);
+// Add an intersection observer 
+const callback = (items, observer) => {
+  items.forEach((item) => {
+    if (item.isIntersecting) {
+      loadImages(item.target);
+      observer.unobserve(item.target);
+    }
+  });
+};
+
+// Set up the options
+let options = {
+  threshold: 0.1
+};
+
+// Create an observer
+const observer = new IntersectionObserver(callback, options);
+
+// Register each image with the intersection observer
+imagesToLoad.forEach((img) => {
+  observer.observe(img);
 });
-}
-
-
 
 const date1 = document.querySelector("#date1");
 
