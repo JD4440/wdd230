ae0a2dda5baed7b59ef46429c48cd052c8e890df
JD4440 <sajkmj@outlook.com>
2023-06-02 22:18:54 -0600

    update css

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -20,3 +20,64 @@ if (today === 1 || today === 2) {
     document.getElementById("banner").style.display = "block";
 }
 
+//Lazy Load Images
+// Gather the images to load
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+
+// Set up the load images function which switches the src and the data-src attributes.
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+
+// Add an intersection observer 
+const callback = (items, observer) => {
+  items.forEach((item) => {
+    if (item.isIntersecting) {
+      loadImages(item.target);
+      observer.unobserve(item.target);
+    }
+  });
+};
+
+// Set up the options
+let options = {
+  threshold: 0.1
+};
+
+// Create an observer
+const observer = new IntersectionObserver(callback, options);
+
+// Register each image with the intersection observer
+imagesToLoad.forEach((img) => {
+  observer.observe(img);
+});
+
+//Number of Visits
+// number of visits
+
+const visitsDisplay = document.querySelector("#num-visits");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+function displayNumVisits (visitsDisplay, numVisits) {
+    if (!visitsDisplay) {
+        return;
+    } else {
+        if (numVisits != 0) {
+            visitsDisplay.textContent = numVisits;
+        } else {
+            visitsDisplay.textContent = `This is your first visit!`;
+        }
+    }
+}
+
+displayNumVisits(visitsDisplay, numVisits);
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
+
