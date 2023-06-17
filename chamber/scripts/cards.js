0c470bb9ebb9300caa4150257625cf9587b79f87
JD4440 <sajkmj@outlook.com>
2023-06-17 16:25:34 -0600

    upate

--- chamber/scripts/cards.js
+++ chamber/scripts/cards.js
@@ -14,18 +14,14 @@ const display = document.querySelector("cards");
 
 // The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
 
+listbutton.addEventListener("click", () => {
+  display.classList.add("list");
+})
+
 gridbutton.addEventListener("click", () => {
-	// example using arrow function
-	display.classList.add("grid");
-	display.classList.remove("list");
-});
+  display.classList.remove("list");
+})
 
-listbutton.addEventListener("click", showList); // example using defined function
-
-function showList() {
-	display.classList.add("list");
-	display.classList.remove("grid");
-}
 
   //business cards
   getBusinessData();
