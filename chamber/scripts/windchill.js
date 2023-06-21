a31724c6d8a058245144a42ca4b147e1de511d6c
JD4440 <sajkmj@outlook.com>
2023-06-20 19:05:23 -0600

    update

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,3 +1,47 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+// lat: 53.90017000, lon: -110.90170000 of ELk Point Alberta
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=53.90017000&lon=-110.90170000&units=metric&appid=93c38752305ca77c6133640779c32982';
+
+//const url = 'http://api.openweathermap.org/data/2.5/weather?q={city}&appid=93c38752305ca77c6133640779c32982';
+
+async function apiFetch() {
+  try {
+      const response = await fetch(url);
+      if (response.ok) {
+          const data = await response.json();
+          // console.log(data);
+          displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+apiFetch();
+
+function displayResults(weatherData) {
+  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`; 
+
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  const desc = weatherData.weather[0].description;
+
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = desc;
+}
+
+
+
+
+
+
+
 // Temp is Celsius, Windseep is kph
 //<=50°F(10°C) and >3.0mph(4.83kmh)
 
