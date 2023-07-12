bb8a5b3bf2b03da4b9900ec300692dac5dbf40d3
JD4440 <sajkmj@outlook.com>
2023-07-11 20:04:51 -0600

    udpate

--- bountiful/scripts/drinkorder.js
+++ bountiful/scripts/drinkorder.js
@@ -23,7 +23,7 @@ async function fruitFetch(url) {
 
 fruitFetch(url)
 
-function drinkCard() {
+async function  drinkCard() {
   console.log("here")
     // Get the current date
     const orderDate = new Date().toDateString();
@@ -33,20 +33,35 @@ function drinkCard() {
     const fruit1 = document.getElementById('fruit1').value;
     const fruit2 = document.getElementById('fruit2').value;
     const fruit3 = document.getElementById('fruit3').value;
+    const instructions = document.getElementById('instructions').value;
 
     // Calculate the total nutrition values based on selected fruits
+
+    let data
+    try {
+      const  response = await fetch(url);
+      if (response.ok) {
+          data = await response.json();
+          console.log(data)
+      }
+    }catch(error){
+        console.log(error)
+      }
     const selectedFruits = [fruit1, fruit2, fruit3];
     let totalCarbs = 0, totalProtein = 0, totalFat = 0, totalSugar = 0, totalCalories = 0;
     selectedFruits.forEach(fruitName => {
       const fruit = data.find(fruit => fruit.name === fruitName);
       if (fruit) {
-        totalCarbs += fruit.carbs;
-        totalProtein += fruit.protein;
-        totalFat += fruit.fat;
-        totalSugar += fruit.sugar;
-        totalCalories += fruit.calories;
+      
+        totalCarbs += fruit.nutritions.carbohydrates;
+        totalProtein += fruit.nutritions.protein;
+        totalFat += fruit.nutritions.fat;
+        totalSugar += fruit.nutritions.sugar;
+        totalCalories += fruit.nutritions.calories;
         }
       });
+
+      console.log(totalCarbs,totalProtein,totalFat,totalSugar,totalCalories)
     
     // Generate the formatted output
     const output = `
@@ -58,16 +73,27 @@ function drinkCard() {
       <p><strong>Selected Fruits:</strong> ${fruit1 + " " + fruit2 + " " + fruit3}</p>
       <p><strong>Special Instructions:</strong> ${instructions}</p>
       <h2>Total Nutrition:</h2>
-      <p><strong>Carbohydrates:</strong> ${totalCarbs}g</p>
-      <p><strong>Protein:</strong> ${totalProtein}g</p>
-      <p><strong>Fat:</strong> ${totalFat}g</p>
-      <p><strong>Sugar:</strong> ${totalSugar}g</p>
-      <p><strong>Calories:</strong> ${totalCalories}kcal</p>
+      <p><strong>Carbohydrates:</strong> ${totalCarbs.toFixed(2)}g</p>
+      <p><strong>Protein:</strong> ${totalProtein.toFixed(2)}g</p>
+      <p><strong>Fat:</strong> ${totalFat.toFixed(2)}g</p>
+      <p><strong>Sugar:</strong> ${totalSugar.toFixed(2)}g</p>
+      <p><strong>Calories:</strong> ${totalCalories.toFixed(2)}kcal</p>
     `;
       // Display the output
       document.getElementById('output').innerHTML = output;
+
+      //Count number of orders
+      let numOrders = parseInt(localStorage.getItem("num-orders") || 0)+1;
+      localStorage.setItem("num-orders", numOrders);
+
+      
+    
 }
 
+
+
 document.querySelector("#drink-card-submit").addEventListener("click",drinkCard)
+
+
 
 
--- bountiful/scripts/weather.js
+++ bountiful/scripts/weather.js
@@ -36,7 +36,34 @@ function displayResults(weatherData) {
   captionDesc.textContent = desc;
 }
 
+// function to get thre day forecast
 
+async function threeDay () {
+   let data
+   // Get the folloing three days
+   let forecastDate = new Date()
+   let arrayDays = [new Date(forecastDate.setDate(forecastDate.getDate()+1)),
+                    new Date(forecastDate.setDate(forecastDate.getDate()+1)),
+                    new Date(forecastDate.setDate(forecastDate.getDate()+1))
+                    ]
+  console.log(arrayDays)
+    try {
+        const response = await fetch(threedayforecast);
+        if (response.ok) {
+            data = await response.json();
+            // console.log(data);
+            // displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+    let forecastObjects = []
+    forecastObjects = data.list.filter((item)=> arrayDays.includes(new Date(item.dt_txt)))
+    console.log(forecastObjects)
+}
+threeDay()
 
 apiFetch();
 
